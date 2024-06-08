d999c5e25007fc83d402ed7fbbb33aef5b591870
Hari Upreti <hariupreti679@gmail.com>
2024-06-08 19:04:57 +0545

    Added controller and routes for task

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [UserController::class, 'showForm'])->name('username.form');
+Route::post('/username', [UserController::class, 'processForm'])->name('username.process');
+Route::get('/next', [UserController::class, 'nextPage'])->name('next.page');
